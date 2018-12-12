e7ae8fba606838a75c9f4450f178871f5918e049
Jeremy Warren <jwarren@bp-3.com>
2018-12-12 15:06:14 -0600

    Initialize Camunda Spring Boot project

---
+++ src/main/java/com/bp3/oss/bp3springbootcamunda/Bp3SpringBootCamundaApplication.java
@@ -0,0 +1,14 @@
+package com.bp3.oss.bp3springbootcamunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bp3SpringBootCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bp3SpringBootCamundaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bp3/oss/bp3springbootcamunda/Bp3SpringBootCamundaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bp3.oss.bp3springbootcamunda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bp3SpringBootCamundaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
